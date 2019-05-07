1bc23912a054c214e09457a068082cbf3a8f3331
Alex Smirnov <sas2job@gmail.com>
2019-05-07 09:57:51 +0300

    Add heading and text

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
     def about
+        @heading = "Page about Us!"
+        @text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non enim viverra, accumsan nisl eget, volutpat lacus."
     end
 end
